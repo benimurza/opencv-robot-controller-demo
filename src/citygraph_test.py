27c0f42279cc6d47032303542b6e82d3e7cd76ad
jpo2433 <jasmin.pogatschneg@students.fhv.at>
2018-02-23 15:27:04 +0100

    automatic traffic light algorithm

--- src/citygraph_test.py
+++ src/citygraph_test.py
@@ -1,5 +1,9 @@
 from citygraph import CityGraph
+from trafficlightcontroller import TrafficlightController
 
 graph = CityGraph()
 print(graph.get_shortest_path('D1N', 'D3S'))
 print(graph.get_next_position('D1N', 'D3S'))
+
+c = TrafficlightController()
+c.start_automatic_trafficlight_algorithm()
