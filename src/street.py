a6065e82111970e6dc89edf9536bb70718ac6a59
benimurza <benimurza@yahoo.com>
2018-02-23 15:28:38 +0100

    Initialize road component list in street

--- src/street.py
+++ src/street.py
@@ -1,14 +1,12 @@
 class Street:
     def __init__(self):
+        # Each street has at least one road component
         self.road_component_list = list()
         # Traffic light number at the end of the street (corresponding to this street)
         self.traffic_light_number = None
 
         # Name, as string
         self.street_name = None
-
-        # Each street has at least one road component
-        self.road_component_list = None
 
     def add_road_component(self, road_component):
         self.road_component_list.append(road_component)
