c7eafbb77cd15ee18afabd921232857d75dd670f
benimurza <benimurza@yahoo.com>
2018-02-23 15:09:11 +0100

    Refactor street

--- src/street.py
+++ src/street.py
@@ -1,23 +1,14 @@
 class Street:
-    # Tolerance in pixels for the X axis
-    tolerance_y_axis = 3
-
-    # Traffic light number at the end of the street (corresponding to this street)
-    traffic_light_number = None
-
-    # Location
-    start_point = None
-    end_point = None
-    heading = None
-
-    # Name, as string
-    street_name = None
-
-    # Each street has at least one road component
-    road_component_list = None
-
     def __init__(self):
         self.road_component_list = list()
+        # Traffic light number at the end of the street (corresponding to this street)
+        self.traffic_light_number = None
+
+        # Name, as string
+        self.street_name = None
+
+        # Each street has at least one road component
+        self.road_component_list = None
 
     def add_road_component(self, road_component):
         self.road_component_list.append(road_component)
