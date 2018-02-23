40863e8a78db55df5b7335a8caba2d0d56cc7c73
jpo2433 <jasmin.pogatschneg@students.fhv.at>
2018-02-23 10:09:54 +0100

    add get next position to citygraph

--- src/citygraph.py
+++ src/citygraph.py
@@ -5,7 +5,6 @@ city_builder = CityBuilder()
 
 
 class CityGraph:
-
     dynamicGraph = nx.DiGraph()
     dyPath = dict()
 
@@ -15,9 +14,13 @@ class CityGraph:
 
         for key, values in city_builder.adjacent_street_names.items():
             for item in values:
-                self.dynamicGraph.add_edge(key,item)
+                self.dynamicGraph.add_edge(key, item)
 
         self.dyPath = dict(nx.all_pairs_shortest_path(self.dynamicGraph))
 
     def get_shortest_path(self, source, dest):
         return self.dyPath[source][dest]
+
+    def get_next_position(self, police, robber):
+        path = self.dyPath[police][robber]
+        return path[1]
--- src/citygraph_test.py
+++ src/citygraph_test.py
@@ -2,4 +2,4 @@ from citygraph import CityGraph
 
 graph = CityGraph()
 print(graph.get_shortest_path('D1N', 'D3S'))
-
+print(graph.get_next_position('D1N', 'D3S'))
